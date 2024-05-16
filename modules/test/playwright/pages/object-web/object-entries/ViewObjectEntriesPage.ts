0dd3f99a5d2d2c833aaad1d12a930218467bfae9
Larissa Cerqueira <larissa.cerqueira.ribeiro@gmail.com>
2024-05-16 18:27:20 -0300

    LPD-25570 Create test to cover object entry with multiple field types

--- modules/test/playwright/pages/object-web/object-entries/ViewObjectEntriesPage.ts
+++ modules/test/playwright/pages/object-web/object-entries/ViewObjectEntriesPage.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {Locator, Page, expect} from '@playwright/test';
+import {FrameLocator, Locator, Page, expect} from '@playwright/test';
 
 import {PORTLET_URLS} from '../../../utils/portletUrls';
 
@@ -12,7 +12,11 @@ export class ViewObjectEntriesPage {
 	readonly backButton: Locator;
 	readonly duplicateEntryErrorMessage: Locator;
 	readonly page: Page;
+	readonly richTextBodyIFrame: FrameLocator;
+	readonly richTextInputBox: Locator;
 	readonly saveObjectEntryButton: Locator;
+	readonly selectFileButton: Locator;
+	readonly selectFileIframe: FrameLocator;
 	readonly successMessage: Locator;
 
 	constructor(page: Page) {
@@ -24,7 +28,17 @@ export class ViewObjectEntriesPage {
 			'Error:The field values are already in use. Please choose unique values.'
 		);
 		this.page = page;
+		this.richTextBodyIFrame = page
+			.getByRole('application', {
+				name: /Rich Text Editor, _com_liferay_object_web_internal_object_definitions_portlet_ObjectDefinitionsPortlet_.*_ddm\$\$.*\$.*\$en_US/,
+			})
+			.frameLocator('iframe');
+		this.richTextInputBox = this.richTextBodyIFrame.getByRole('textbox');
 		this.saveObjectEntryButton = page.getByRole('button', {name: 'Save'});
+		this.selectFileButton = page.getByRole('button', {name: 'Select File'});
+		this.selectFileIframe = page.frameLocator(
+			'iframe[title="Select File"]'
+		);
 		this.successMessage = page.getByText(
 			'Your request completed successfully.'
 		);
@@ -35,8 +49,48 @@ export class ViewObjectEntriesPage {
 		await expect(this.duplicateEntryErrorMessage).toBeVisible();
 	}
 
+	async fillDateEntry(fieldValue: string) {
+		await this.page.getByPlaceholder('__/__/____').click();
+		await this.page.getByPlaceholder('__/__/____').fill(fieldValue);
+	}
+
 	async fillObjectEntry(fieldName: string, fieldValue: string) {
-		await this.page.getByLabel(fieldName).fill(fieldValue);
+		await this.page.getByLabel(fieldName, {exact: true}).fill(fieldValue);
+	}
+
+	async fillRichTextEntry(fieldValue: string) {
+		await this.page.waitForSelector('iframe');
+		await this.richTextInputBox.fill(fieldValue);
+		await this.richTextInputBox.click({button: 'left'});
+		await this.richTextInputBox.press('Backspace');
+	}
+
+	async selectDropdownItem(fieldName: string, optionName: string) {
+		await this.page.getByLabel(fieldName).click();
+		await this.page.getByRole('option', {name: optionName}).click();
+	}
+
+	async selectFileFromDocumentsAndMedia() {
+		await this.selectFileButton.click();
+		await this.selectFileIframe
+			.getByRole('link', {name: 'Sites and Libraries'})
+			.click();
+		await this.selectFileIframe
+			.getByRole('link', {name: 'Liferay DXP'})
+			.click();
+		await this.selectFileIframe
+			.getByRole('link', {name: 'Provided by Liferay'})
+			.click();
+		await this.selectFileIframe
+			.locator(
+				'[id="_com_liferay_item_selector_web_portlet_ItemSelectorPortlet_repositoryEntriesSearchContainer"] img'
+			)
+			.first()
+			.click();
+	}
+
+	async assertObjectEntries(entryValue: string) {
+		await this.page.locator('div.dnd-td').getByText(entryValue);
 	}
 
 	async goto(objectDefinitionId: number, siteUrl?: Site['friendlyUrlPath']) {
