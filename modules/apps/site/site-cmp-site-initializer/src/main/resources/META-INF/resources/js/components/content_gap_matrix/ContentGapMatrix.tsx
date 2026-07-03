68b02639c8afb0cdfd4f197252a43456ed70af3e
Larissa Ribeiro <larissa.ribeiro@liferay.com>
2026-07-03 09:55:46 -0300

    LPD-93350 Add the Content Coverage Matrix grid and header

---
+++ modules/apps/site/site-cmp-site-initializer/src/main/resources/META-INF/resources/js/components/content_gap_matrix/ContentGapMatrix.tsx
@@ -0,0 +1,33 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import React from 'react';
+
+import ContentGapMatrixGrid from './ContentGapMatrixGrid';
+import {MatrixData} from './types';
+
+import './ContentGapMatrix.scss';
+
+export default function ContentGapMatrix({data}: {data: MatrixData}) {
+	return (
+		<div className="lfr-cmp__content-gap-matrix">
+			<div className="lfr-cmp__content-gap-matrix-intro">
+				<h5 className="lfr-cmp__content-gap-matrix-title">
+					{Liferay.Language.get(
+						'amount-of-assets-per-persona-x-funnel-stage'
+					)}
+				</h5>
+
+				<p className="lfr-cmp__content-gap-matrix-description">
+					{Liferay.Language.get(
+						'this-report-provides-a-breakdown-of-all-project-assets-by-persona-and-funnel-stage'
+					)}
+				</p>
+			</div>
+
+			<ContentGapMatrixGrid data={data} />
+		</div>
+	);
+}
